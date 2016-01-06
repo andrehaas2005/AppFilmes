063a95118c14bd5096cde16e884e9f18e939a859
andrehaas2005 <andrehaas2005@gmail.com>
2016-01-06 18:01:11 -0200

    first commit

---
+++ AppFilmes/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDbLib.Client;
+using TMDbLib.Objects.Movies;
+
+namespace AppFilmes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var chave = System.Web.Configuration.WebConfigurationManager.AppSettings["chaveAcesso"].ToString();
+            var tmDbClient = new TMDbClient(chave);
+            var filmes = tmDbClient.GetMovieList(MovieListType.Popular);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
